8bff95e5d784a4255496e38a8758aa02b21a21fa
lsj8367 <lsj8367@naver.com>
2021-12-10 13:16:43 +0900

    :wrench: 캐시 의존성 추가

--- src/main/java/io/github/lsj8367/CacheApplication.java
+++ src/main/java/io/github/lsj8367/CacheApplication.java
@@ -2,8 +2,10 @@ package io.github.lsj8367;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CacheApplication {
 
     public static void main(String[] args) {
